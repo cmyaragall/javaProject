a86a46512c5770f42a6d769bf7c7717f49eebcb0
WildJasmine <WildJasmine@LAPTOP-COS67E9J>
2020-05-28 13:17:30 +0530

    This my First code commit

---
+++ mavenproject1/src/main/java/FirstSource/First.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FirstSource;
+
+/**
+ *
+ * @author WildJasmine
+ */
+public class First {
+     public static void main(String args[]){
+        System.out.println("Hi this is my first Java class");
+    }
+}
