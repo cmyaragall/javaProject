7ea90d38591af63b6df5e0557fcf2b5077112571
WildJasmine <WildJasmine@LAPTOP-COS67E9J>
2020-05-28 15:40:40 +0530

    created a new package SecondSource and added SecondChanges class

---
+++ mavenproject1/src/main/java/SecondSource/SecondChanges.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SecondSource;
+
+/**
+ *
+ * @author WildJasmine
+ */
+public class SecondChanges {
+    int i,j;
+    public SecondChanges(int k,int j){
+        i=k;
+        this.j=j;
+        System.out.println(i);
+        System.out.println(j);
+        
+    }
+    
+    public static void main(String[] args) {
+        SecondChanges s=new SecondChanges(11, 22);
+        
+    }
+    
+}
